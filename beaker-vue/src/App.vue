1d976dfbdf88b7309eb645f63ba8ee8270354ad3
Matthew Printz <matt@jataware.com>
2024-01-31 14:22:43 -0600

    Set page name and cleanup tabs

--- beaker-vue/src/App.vue
+++ beaker-vue/src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, onBeforeMount } from 'vue';
 import { BeakerSession } from 'beaker-kernel';
 import BeakerNotebook from './components/BeakerNotebook.vue';
 
@@ -38,6 +38,9 @@ rawSession.sessionReady.then(() => {
 
 });
 
+onBeforeMount(() => {
+  document.title = "Beaker Development Interface"
+});
 
 const beakerSession = reactive(rawSession);
 
