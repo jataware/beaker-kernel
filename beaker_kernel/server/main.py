0ed0597e17cd876161895ee50590b0793efdb88f
Matthew Printz <matt@jataware.com>
2024-02-13 14:59:09 -0600

    Finishing off switch

--- beaker_kernel/server/main.py
+++ beaker_kernel/server/main.py
@@ -1,16 +1,16 @@
+import datetime
+import json
 import logging
 import os
 from typing import Dict
 
 from jupyter_server.base.handlers import JupyterHandler
 from jupyter_server.extension.handler import (
-    ExtensionHandlerJinjaMixin,
     ExtensionHandlerMixin,
 )
 from jupyter_server.utils import url_path_join as ujoin
 from jupyterlab_server import LabServerApp
-from tornado import httputil
-from tornado.web import Application
+from tornado.web import StaticFileHandler, RedirectHandler
 
 from beaker_kernel.lib.autodiscovery import autodiscover
 from beaker_kernel.lib.context import BaseContext
@@ -24,7 +24,44 @@ version = "0.0.1"
 
 
 def _jupyter_server_extension_points():
-    return [{"module": __name__, "app": BeakerJupyterApp}]
+    return [{"module": "beaker_kernel.server.main", "app": BeakerJupyterApp}]
+
+
+class NotebookHandler(ExtensionHandlerMixin, JupyterHandler):
+
+    def get(self):
+        return self.write(json.dumps(notebook_content))
+
+    def post(self):
+        global notebook_content
+        notebook_content = self.get_json_body()
+        notebook_content["lastSaved"] = datetime.datetime.utcnow().isoformat()
+        return self.write(json.dumps(notebook_content))
+
+
+
+class ConfigHandler(ExtensionHandlerMixin, JupyterHandler):
+    """
+    Provide config via an endpoint
+    """
+
+    def get(self):
+        """
+        """
+        base_url = f"{self.request.protocol}://{self.request.host}"
+        if self.request.protocol.endswith("s"):
+            ws_proto = "wss"
+        else:
+            ws_proto = "ws"
+        ws_url = f"{ws_proto}://{self.request.host}"
+
+        config_data = {
+            "appUrl": os.environ.get("APP_URL", base_url),
+            "baseUrl": os.environ.get("JUPYTER_BASE_URL", base_url),
+            "wsUrl": os.environ.get("JUPYTER_WS_URL", ws_url),
+            "token": os.environ.get("JUPYTER_TOKEN", "89f73481102c46c0bc13b2998f9a4fce"),
+        }
+        return self.write(config_data)
 
 
 class ContextHandler(ExtensionHandlerMixin, JupyterHandler):
@@ -63,16 +100,25 @@ class ContextHandler(ExtensionHandlerMixin, JupyterHandler):
 
 
 class BeakerJupyterApp(LabServerApp):
-    name = __name__
-    load_other_extensions = False
+    name = "beaker_kernel"
+    load_other_extensions = True
     app_name = "Beaker Jupyter App"
     app_version = version
     allow_origin = "*"
     open_browser = False
+    extension_url = "/"
 
     def initialize_handlers(self):
         """Bypass initializing the default handler since we don't need to use the webserver, just the websockets."""
         self.handlers.append(("/contexts", ContextHandler))
+        self.handlers.append(("/config", ConfigHandler))
+        self.handlers.append(("/notebook", NotebookHandler))
+        self.handlers.append((r"(/?)", StaticFileHandler, {"path": os.path.join(HERE, "ui"), "default_filename": "index.html"}))
+        self.handlers.append((r"/index.html", StaticFileHandler, {"path": os.path.join(HERE, "ui"), "default_filename": "index.html"}))
+        self.handlers.append((r"/favicon.ico", StaticFileHandler, {"path": os.path.join(HERE, "ui")}))
+        self.handlers.append((r"/static/(.*)", StaticFileHandler, {"path": os.path.join(HERE, "ui", "static")}))
+        self.handlers.append((r"/themes/(.*)", StaticFileHandler, {"path": os.path.join(HERE, "ui", "themes")}))
+        self.handlers.append((r"/dev_ui/?(.*)", RedirectHandler, {"url": r"/{0}"}))
 
     def initialize_settings(self):
         # Override to allow cross domain websockets
