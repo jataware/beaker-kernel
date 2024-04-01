ed4d7692dbc424d6ec78631410bdbdc03d46c597
Five Grant <5@fivegrant.com>
2024-04-01 17:45:51 -0500

    Refactor into simpler single script

--- eval_app/app/index.ts
+++ eval_app/app/index.ts
@@ -1,17 +1,18 @@
-import { BeakerSession } from 'beaker-ts';
+import { promises as fs, Dirent } from 'fs';
+import path from 'path';
+import { BeakerSession } from 'beaker-kernel';
 
-import { get_benchmarks, Scenario } from './benchmarking.js'
+import { baseUrl, token } from './settings.json'
 
 
-function createSession(baseUrl:string): BeakerSession {
-  // TODO: Expose settings instead of baseUrl
+async function evalNotebook(contextName: string, benchmark: any) {
   const settings = {
-    baseUrl: baseUrl,
+    baseUrl,
     appUrl: baseUrl,
     wsUrl: baseUrl.replace("http", "ws"),
-    token: "89f73481102c46c0bc13b2998f9a4fce",
+    token,
   };  
-  return new BeakerSession(
+  const session = new BeakerSession(
     {
       settings,
       name: "Benchmarking",
@@ -20,28 +21,57 @@ function createSession(baseUrl:string): BeakerSession {
       rendererOptions: {renderers: []}
     }
   );
+
+  session.notebook.loadFromIPynb(benchmark);
+  const contextInfo = JSON.parse(session.notebook.cells[0].source.join("\n"));
+  const setupResult = session.sendBeakerMessage(
+      "context_setup_request",
+      {
+        contextName,
+        contextInfo: contextInfo
+      }
+  );
+  const startSuccess = ['error', 'abort'].indexOf(setupResult?.content?.status) === -1;
+  if (!startSuccess) {
+    throw new Error("Failed to start session");
+  };
+
+  for (let cell of session.notebook.cells){
+    if (cell != null){
+      await cell.execute(session);
+    }
+  }
+
+  const resultingNotebook = session.notebook.toIPynb();
+  await session.kernel.shutdown(); // TODO: Refactor when beaker-ts is updated
+  return resultingNotebook;
+
 }
 
 
-async function setScenarioContext (contextName: string, scenario: Scenario, baseUrl: string) {
+async function getBenchmarks(evalDir: string, resultDir: string) {
+  const items = await fs.readdir(evalDir, { withFileTypes: true });
+  const contexts = items.filter((item: Dirent) => item.isDirectory());
+  await fs.mkdir(resultDir);
+  contexts.map(async (context:Dirent)=>{
+    const resultPath = path.join(resultDir, context.name);
+    await fs.mkdir(resultPath);
 
-    const session = createSession("localhost:8080")
-    const result = await session.sendBeakerMessage(
-        "context_setup_request",
-        {
-          contextName,
-          language: scenario.language, 
-          contextInfo: scenario.contextInfo
-        }
-    );
+    const contextPath = path.join(evalDir, context.name);
+    const unfilterFiles = await fs.readdir(contextPath, { withFileTypes: true });
+    const files = unfilterFiles.filter(file => file.name.length <= 6 || file.name.slice(0, file.name.length - 6) !== ".ipynb")
 
-    const startSuccess = ['error', 'abort'].indexOf(result?.content?.status) === -1  
-    return startSuccess
+    files.map( async (file) => {
+      const filePath = path.join(contextPath, file.name);
+      const notebook = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+      const result = evalNotebook(context.name, notebook)
+      const fileContents = JSON.stringify(result);
+      await fs.writeFile(path.join(resultPath, file.name), fileContents);
+    })
+    
+  });
+  await Promise.all(contexts)
 }
 
-
-
-
-get_benchmarks("./evaluations")
-  .then(console.log);
+getBenchmarks("./evaluations", "./result")
 
