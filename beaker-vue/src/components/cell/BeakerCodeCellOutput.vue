2ed24cd423fb3b21c9ee745d845b55b4c7eac458
Matthew Printz <matt@jataware.com>
2024-09-09 12:15:13 -0500

    Added collapse to cells and outputs

--- beaker-vue/src/components/cell/BeakerCodeCellOutput.vue
+++ beaker-vue/src/components/cell/BeakerCodeCellOutput.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="code-cell-output jp-RenderedText">
-        <div v-for="output of props.outputs" :key="output">
+        <div class="code-cell-output-box" :class="{'collapsed-output': output.metadata?.collapsed}" v-for="output of props.outputs" :key="output">
+            <div class="output-collapse-box" @click.capture.stop.prevent="collapseOutput(output)"></div>
             <div v-if="output.output_type == 'stream'" :class="output.output_type">{{ output.text }}</div>
             <BeakerMimeBundle
                 v-else-if="['display_data', 'execute_result'].includes(output.output_type)"
@@ -16,14 +17,18 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, inject } from "vue";
+import { ref, defineProps, inject } from "vue";
 import BeakerMimeBundle from "../render/BeakerMimeBundle.vue";
-// import { IMimeBundle } from 'beaker-kernel';
 
 const props = defineProps([
     "outputs",
     "busy"
 ]);
+
+const collapseOutput = (output) => {
+    const metadata = ref(output?.metadata || (output.metadata = {}))
+    metadata.value.collapsed = !metadata.value.collapsed;
+}
 
 const rebundleError = (errorOutput) => {
     const traceback = (Array.isArray(errorOutput.traceback) ? errorOutput.traceback?.join('\n') : errorOutput.traceback?.toString());
@@ -42,7 +47,9 @@ const rebundleError = (errorOutput) => {
 @import url('@jupyterlab/notebook/style/base.css');
 
 .code-cell-output {
-    padding: 1rem;
+    --collapse-height: 3em;
+
+    padding: 0 0.5em 0.5em 0;
     background-color: var(--surface-c);
     position: relative;
     overflow-x: auto;
@@ -52,6 +59,35 @@ const rebundleError = (errorOutput) => {
             overflow-wrap: break-word;
         }
     }
+}
+
+.collapsed-output {
+    max-height: var(--collapse-height);
+    overflow-y: auto;
+
+    .output-collapse-box {
+        background-color: var(--surface-border);
+    }
+}
+
+.code-cell-output-box {
+    margin-top: 0.5em;
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+}
+
+.output-collapse-box {
+    min-width: 10px;
+    width: 10px;
+    border: 1px inset var(--surface-border);
+    cursor: pointer;
+
+    &:hover {
+        background-color: color(from var(--surface-border) srgb r g b / 0.5);
+    }
+    margin-right: 0.5em;
+
 }
 
 .stdout {
