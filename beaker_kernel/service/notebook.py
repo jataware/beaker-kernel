15c793a98081778b612035f8366537b58ac25cf2
Matthew Printz <matt@jataware.com>
2025-07-11 19:00:27 -0600

    Further implementation of authentication

--- beaker_kernel/service/notebook.py
+++ beaker_kernel/service/notebook.py
@@ -1,3 +1,6 @@
+import os
+
+from beaker_kernel.lib.config import config
 from beaker_kernel.service.base import BaseBeakerServerApp
 
 
@@ -6,7 +9,9 @@ def _jupyter_server_extension_points():
 
 
 class BeakerNotebookApp(BaseBeakerServerApp):
-    pass
+    def __init__(self, **kwargs):
+        os.environ.setdefault("JUPYTER_TOKEN", config.jupyter_token)
+        super().__init__(**kwargs)
 
 
 if __name__ == "__main__":
