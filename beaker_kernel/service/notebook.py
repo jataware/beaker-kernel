2b6067486ddc288e34fccaa35a48a783303e2e7c
Matthew Printz <matt@jataware.com>
2025-07-16 16:43:13 -0600

    Added session culling when idle, fixed auth issues

--- beaker_kernel/service/notebook.py
+++ beaker_kernel/service/notebook.py
@@ -2,6 +2,7 @@ import os
 
 from beaker_kernel.lib.config import config
 from beaker_kernel.service.base import BaseBeakerServerApp
+from beaker_kernel.service.auth.notebook import NotebookAuthorizer, NotebookIdentityProvider
 
 
 def _jupyter_server_extension_points():
@@ -9,6 +10,12 @@ def _jupyter_server_extension_points():
 
 
 class BeakerNotebookApp(BaseBeakerServerApp):
+
+    app_traits = {
+        "authorizer_class": NotebookAuthorizer,
+        "identity_provider_class": NotebookIdentityProvider,
+    }
+
     def __init__(self, **kwargs):
         os.environ.setdefault("JUPYTER_TOKEN", config.jupyter_token)
         super().__init__(**kwargs)
