e7a6f1a24251e1c3fbe1add9943ba5b9d5c01622
J.Quiles <joel@radcoding.com>
2025-08-26 11:40:34 -0400

    Regression- need to refactor to support both

--- beaker-vue/src/components/notebook/BeakerNotebookPanel.vue
+++ beaker-vue/src/components/notebook/BeakerNotebookPanel.vue
@@ -154,7 +154,10 @@ defineExpose({
     position: relative;
     flex: 1;
     background-color: var(--p-surface-b);
-    z-index: 50;
+    // TODO fixme
+    // disable for query cell sticky..
+    // enable for svh beaker placeholder..
+    // z-index: 50;
     overflow: auto;
     width: 100%;
     height: 100%;
